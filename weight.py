44977f24b71d9acfc24811f2d395eb5846bd1b1f
mn1del <davidleech@gmail.com>
2018-12-26 21:45:01 +0000

    add weight calibration

--- weight.py
+++ weight.py
@@ -13,14 +13,14 @@ class HX(HX711.sensor):
     def __init__(self, pi, DATA=5, CLOCK=6, mode=HX711.CH_B_GAIN_32, callback=cbf):
         super().__init__(pi, DATA=5, CLOCK=6, mode=HX711.CH_B_GAIN_32, callback=cbf)
 
-    def print10x(self, val):
-        print("10x: {}".format(val * 10))
+    def zero(self, zero_reading):
+        self.offset = reading
 
-    def saven(self, n):
-        self.n = n
+    def calibrate(self, known_weight, reading):
+        self.slope = known_weight/(reading - self.offset)
 
-    def printnx(self, val):
-        print("nx: {}".format(self.n * val))
+    def get_weight(self, reading):
+        return self.slope * (reading - self.offset)
 
 if __name__ == "__main__":
 
@@ -70,12 +70,18 @@ if __name__ == "__main__":
 
       print("Cancel callback and read manually")
 
+      input('Put known weight on the scale and then press Enter')
+
       c, mode, reading = s.get_reading()
+      s.zero(reading)
+
+      known_weight_grams = input('Write how many grams it was and press Enter: ')
+      c, mode, reading = s.get_reading()
+      s.calibrate(known_weight_grams, reading)
+      print("Calibration slope: {}, Offset: {}",format(s.slope, s.offset))
 
       stop = time.time() + 3600
-
-      s.saven(0.1)
-      s.print10x(10)
+      
 
       while time.time() < stop:
 
@@ -84,7 +90,7 @@ if __name__ == "__main__":
          if count != c:
             c = count
             print("{} {} {}".format(count, mode, reading))
-            s.printnx(reading)
+            print("Weight: {} g".format(s.get_weight(reading)))
 
          time.sleep(0.05)
 
