b23e522ecfee85e9553918f73a6031c1a4b13202
mn1del <davidleech@gmail.com>
2018-12-28 20:11:45 +0000

    streamline pigpio code on new pigpio branch

--- weight.py
+++ weight.py
@@ -7,7 +7,7 @@ import HX711
 
 
 def cbf(count, mode, reading):
-    print("callback: {}, {}, {}".format(count, mode, reading))
+    print("count: {}, mode: {}, reading: {}".format(count, mode, reading))
 
 class HX(HX711.sensor):
     def __init__(self, pi, DATA=5, CLOCK=6, mode=HX711.CH_B_GAIN_32, callback=cbf):
@@ -30,49 +30,15 @@ if __name__ == "__main__":
    if not pi.connected:
       exit(0)
 
-   s = HX(pi, DATA=5, CLOCK=6, mode=HX711.CH_B_GAIN_32, callback=cbf)
+   s = HX(pi, DATA=5, CLOCK=6, mode=HX711.CH_A_GAIN_128, callback=None)
    #s = HX711.sensor(
    #   pi, DATA=5, CLOCK=6, mode=HX711.CH_B_GAIN_32, callback=cbf)
 
    try:
-      print("start with CH_B_GAIN_32 and callback")
-
       time.sleep(1)
 
-      s.set_mode(HX711.CH_A_GAIN_64)
-
-      print("Change mode to CH_A_GAIN_64")
-
-      time.sleep(1)
-
-      s.set_mode(HX711.CH_A_GAIN_128)
-
-      print("Change mode to CH_A_GAIN_128")
-
-      time.sleep(1)
-
-      s.pause()
-
-      print("Pause")
-
-      time.sleep(1)
-
-      s.start()
-
-      print("Start")
-
-      time.sleep(1)
-
-      s.set_callback(None)
-
-      s.set_mode(HX711.CH_A_GAIN_128)
-
-      print("Change mode to CH_A_GAIN_128")
-
-      print("Cancel callback and read manually")
-
+      # Zero
       c, mode, reading = s.get_reading()
-
       while True:
          count, mode, reading = s.get_reading()
 
@@ -83,6 +49,7 @@ if __name__ == "__main__":
 
          time.sleep(0.05)
 
+      # Calibrate to known weight
       known_weight_grams = float(input('Put know weight on the scales and enter weight here: '))
       c, mode, reading = s.get_reading()
       while True:
@@ -96,20 +63,8 @@ if __name__ == "__main__":
 
          time.sleep(0.05)
 
-      c, mode, reading = s.get_reading()
-
-      stop = time.time() + 3600
-
-      while time.time() < stop:
-
-         count, mode, reading = s.get_reading()
-
-         if count != c:
-            c = count
-            print("{} {} {}".format(count, mode, reading))
-            print("Weight: {} g".format(s.get_weight(reading)))
-
-         time.sleep(0.05)
+      # Rely on callback to print data
+      s.set_callback(cbf)
 
    except KeyboardInterrupt:
       pass
