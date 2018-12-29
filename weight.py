6a2684ed99029764c009089e989bec0faf5b3be3
mn1del <davidleech@gmail.com>
2018-12-29 21:53:48 +0000

    debug pigpio

--- weight.py
+++ weight.py
@@ -10,9 +10,8 @@ def cbf(count, mode, reading):
     print("callback: {}, {}, {}".format(count, mode, reading))
 
 class HX(HX711.sensor):
-    def __init__(self, pi, DATA=5, CLOCK=6, mode=HX711.CH_A_GAIN_128, callback=None):
-        print("test")
-        super().__init__(pi=pi, DATA=DATA, CLOCK=CLOCK, mode=mode, callback=callback)
+    #def __init__(self, pi, DATA=5, CLOCK=6, mode=HX711.CH_A_GAIN_128, callback=None):
+    #    super().__init__(pi=pi, DATA=DATA, CLOCK=CLOCK, mode=mode, callback=callback)
 
     def zero(self, zero_reading):
         self.offset = reading
