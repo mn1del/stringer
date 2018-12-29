7b59d474d3fbf79c9a0bed852f7e796303514c0e
mn1del <davidleech@gmail.com>
2018-12-29 20:50:13 +0000

    fix super().__init__() args bug

--- weight.py
+++ weight.py
@@ -11,7 +11,7 @@ def cbf(count, mode, reading):
 
 class HX(HX711.sensor):
     def __init__(self, pi, DATA=5, CLOCK=6, mode=HX711.CH_B_GAIN_32, callback=cbf):
-        super().__init__(pi, DATA=5, CLOCK=6, mode=HX711.CH_B_GAIN_32, callback=cbf)
+        super().__init__(pi, DATA=DATA, CLOCK=CLOCK, mode=mode, callback=callback)
 
     def zero(self, zero_reading):
         self.offset = reading
