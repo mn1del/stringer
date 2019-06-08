5988b09ed7d5aba2bd2fc072ababda8dc28d66a0
mn1del <davidleech@gmail.com>
2019-06-08 15:57:21 +0100

    lower acceleration

--- weight.py
+++ weight.py
@@ -51,7 +51,7 @@ class Stringer():
                 ms1_pin=20, 
                 ms2_pin=16,
                 steps_per_rev=self.stepper_full_steps_per_rev * self.microstep_mode,
-                acceleration=500,
+                acceleration=400,
                 starting_rpm=6,
                 microstep_mode=self.microstep_mode,
                 driver="drv8825")
