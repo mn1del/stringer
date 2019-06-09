3598a206e57dc8a18bd0e033150414d9f604df19
mn1del <davidleech@gmail.com>
2019-06-09 21:32:36 +0100

    debug tensioning

--- weight.py
+++ weight.py
@@ -386,6 +386,7 @@ class Stringer():
         while (self.RUN_THREADS) & (self.MODE == "tensioning"):
             self.target_kgs = max(0,min(500, self.rot.COUNTER))/10
             self.lcd.lcd_string("Target: {:,.1f} kg".format(self.target_kgs), self.lcd.LCD_LINE_1)
+            print("Got here")
             self.lcd.lcd_string("Actual: {:,.1f} kg".format(self.CURRENT_KGS), self.lcd.LCD_LINE_2)
             print("Got here!!!")
         
